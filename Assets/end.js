65764b14e9aa17d60c85eed0b4c5afc719a55e8e
Emma Carr <lorenagracex@gmail.com>
2023-04-10 15:47:30 -0500

    highscores and im losing my mind

--- Assets/end.js
+++ Assets/end.js
@@ -1,15 +1,37 @@
-const initials = document.getElementById('initials');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const initials = document.getElementById("initials");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
-initials.addEventListener('keyup', () => {
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+// console.log(JSON.parse(localStorage.getItem('highScores')));
+// logs the array that is taken from local storage
+console.log(highScore);
+const MAX_HIGH_SCORES = 5;
+
+initials.addEventListener("keyup", () => {
   // console.log(initials.value); logs what is typed in the form
-  saveScoreBtn.disabled = ! initials.value;
-})
+  saveScoreBtn.disabled = !initials.value;
+});
 
 saveHighScore = (e) => {
   e.preventDefault();
   // console.log('clicked save') -> testing save button event
+
+  const score = {
+    score: mostRecentScore,
+    name: initials.value,
+  };
+  highScore.push(score);
+
+  // used to create a sorted array
+  highScore.sort((a, b) => b.score - a.score);
+  // at index 5 cut off everything following it
+  highScore.splice(5);
+
+  localStorage.getItem('highScore', JSON.stringify(highScore));
+  window.location.assign('/');
 };
+
+console.log(highScore);
---
+++ Assets/highscores.js
@@ -0,0 +1,10 @@
+const highScoreList = document.getElementById('highScoreList');
+const highScore = JSON.parse(localStorage.getItem('highScore')) || [];
+// console.log(highScore);
+
+highScoreList.innerHTML = highScore
+  .map((score) => {
+    // map takes an array of items and makes each of those items something else
+    return `<li class="high-score">${score.initials} - ${score.score}</li>`;
+  })
+  .join("");
